3430ea7c309a99b20bf4d5861991aed3948a1dc2
emmameritt <emmameritt@gmail.com>
2024-07-30 20:18:34 -0500

    steps 1-3 complete

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -1,6 +1,9 @@
 // This assignment is inspired by a problem on Exercism (https://exercism.org/tracks/javascript/exercises/etl) that demonstrates Extract-Transform-Load using Scrabble's scoring system. 
 
+const { runCLI } = require("jest");
 const input = require("readline-sync");
+
+let userInput= ""; 
 
 const oldPointStructure = {
   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
@@ -33,26 +36,91 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   userInput = input.question("Let's play some scrabble! Enter a word to score:");
 };
 
-let newPointStructure;
+function simpleScorer(userInput) {
+  let score = 0;
+   for (let i = 0; i < userInput.length; i++){
+    score +=1
+   }
+return score};
 
-let simpleScorer;
+function vowelBonusScorer(userInput) {
+  let score = 0;
+   for (let i = 0; i < userInput.length; i++){
+    if (["a","e","i","o","u"].includes(userInput[i].toLowerCase())){
+      score += 3
+    } else {
+      score +=1
+    }
+   }
+return score
+};
 
-let vowelBonusScorer;
+let newPointStructure = {
+  a: [1],
+  b: [3],
+  c: [3],
+  d: [2],
+  e: [1],
+  f: [4],
+  g: [2],
+  h: [4],
+  i: [1],
+  j: [8],
+  k: [5],
+  l: [1],
+  m: [3],
+  n: [1],
+  o: [1],
+  p: [3],
+  q: [10],
+  r: [1],
+  s: [1],
+  t: [1],
+  u: [1],
+  v: [4],
+  w: [4],
+  x: [8],
+  y: [4],
+  z: [10]
+};
+
+//let simpleScorer;
+
+//let vowelBonusScorer;
 
 let scrabbleScorer;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [ {
+  name: "Simple Scorer",
+  description: "Each letter is worth 1 point.",
+  scorerFunction: simpleScorer,},
 
-function scorerPrompt() {}
+ { name: "Bonus Vowels",
+  description: "Vowels are 3 pts, consonants are 1 pt.",
+  scorerFunction: vowelBonusScorer, },
+
+ { name: "Scrabble Scorer",
+  description: "The traditional scoring algorithm.",
+  scorerFunction: oldScrabbleScorer, }];
+
+function scorerPrompt() {
+  let scoringSelection = input.question("Please select your scoring algorithm as one of the following: 0 for Simpler Scoring, 1 for Vowel Bonus Scoring, or 2 for Classic Scrabble scoring.");  
+   console.log("algorithm name: ", scoringAlgorithms[scoringSelection].name);
+   console.log(`scorerFunction result for ${userInput}:`, scoringAlgorithms[scoringSelection].scorerFunction(userInput));
+  
+}
 
 function transform() {};
 
 function runProgram() {
    initialPrompt();
-   
+    scorerPrompt();
+    //console.log(oldScrabbleScorer(userInput));
+    //simpleScorer(userInput);
+    //vowelBonusScorer(userInput);
 }
 
 // Don't write any code below this line //
